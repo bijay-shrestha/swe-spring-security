ae215c2f46a70217275aa9faef5b065bd5e0d029
bijay-shrestha <8ijay.shrestha@gmail.com>
2022-07-10 14:39:07 -0500

    Initialized Lab 11 Project Default Structure

---
+++ src/main/java/edu/miu/cs/cs425/swespringsecurity/SweSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs425.swespringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SweSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/miu/cs/cs425/swespringsecurity/SweSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs425.swespringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
